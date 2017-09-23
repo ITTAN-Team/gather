d19c04fd396414f44fc760962f03a2b0c4ac358b
shokawasaki <s.kawasaki@noraneko.works>
2017-09-23 19:38:13 +0900

    イベント機能おおかた実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def is_admin? user_id, event_id
+    EventUser.is_admin?(user_id, event_id)
+  end
 end
--- db/migrate/20170921130758_create_event_users.rb
+++ db/migrate/20170921130758_create_event_users.rb
@@ -1,6 +1,7 @@
-class CreateEventHasUsers < ActiveRecord::Migration[5.0]
+class CreateEventUsers < ActiveRecord::Migration[5.0]
   def change
-    create_table :event_has_users do |t|
+    create_table :event_users do |t|
+      t.boolean :admin, default: false
       t.references  :event, foreign_key: true
       t.references  :user, foreign_key: true
       t.timestamps
