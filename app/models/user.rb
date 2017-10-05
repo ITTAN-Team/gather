885b773761f58b92f732dc9129a41fc10f8da022
Yuta Endo <Yuta@YutanoMacBook-Pro.local>
2017-10-05 15:44:52 +0900

    バリデーションの追加とヘッダーのリンクを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
 
   has_many :event_users
   has_many :events, through: :event_users
+
+  validates :email, presence: true
+  validates :email, length: {maximum: 255}
+  validates :encrypted_password, presence: true
+  validates :encrypted_password, length: {maximum: 255}
+  validates :sei, length: {maximum: 255}
+  validates :mei, length: {maximum: 255}
+  validates :image, length: {maximum: 65535}
 end
