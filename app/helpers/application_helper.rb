1b24f54519dbb97d412c1b88a4771ae48601f12d
shokawasaki <s.kawasaki@noraneko.works>
2017-10-07 21:23:16 +0900

    マイページのレイアウト適用

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def is_admin? user_id, event_id
     EventUser.is_admin?(user_id, event_id)
   end
+  def custom_format value, format
+    if value.present? && format.present?
+      value.to_s + format
+    end
+  end
 end
