7c247effbd2b9e8668ab2dffc0e993f9cddb8ee4
Yuta Endo <Yuta@YutanoMacBook-Pro.local>
2017-10-08 16:41:10 +0900

    イベント検索のバグを修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,8 +10,16 @@ class Event < ApplicationRecord
     EventUser.where('user_id = ? AND admin = ?', user_id, true)
   end
 
+  def self.search_sponsorship_events user_id, event_id
+    EventUser.where('user_id = ? AND event_id = ? AND admin = ?', user_id, event_id, true)
+  end
+
   def self.get_participateds_events user_id
     EventUser.where('user_id = ? AND admin = ?', user_id, false)
+  end
+
+  def self.search_participateds_events user_id, event_id
+    EventUser.where('user_id = ? AND event_id = ? AND admin = ?', user_id, event_id, false)
   end
 
   validates :name, {presence: true, length: {maximum: 255}}
