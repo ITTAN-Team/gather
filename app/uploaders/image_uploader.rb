9be47fcf9831960927b2b9abf42bc038af53a93c
shokawasaki <s.kawasaki@noraneko.works>
2017-11-03 17:33:49 +0900

    画像表示のレイアウト修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process scale: [150, 150]
+  process resize_to_fill: [1000, 1000, "#CBCBCB", "Center"]
 
   def scale(width, height)
     # do something
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [150, 150]
+    process resize_to_fit: [500, 500]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
