83bdf505123c37745fc82b87b726f8868a8a89ab
Yuta Endo <Yuta@YutanoMacBook-Pro.local>
2017-11-13 00:31:45 +0900

    s3誤対応の修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    region: 'ap-northeast-1',
-    use_iam_profile: true
+    aws_access_key_id: 'AKIAJEHUIMM5Z7ZRGK2Q',
+    aws_secret_access_key: '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',
+    region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'gather-photo'
