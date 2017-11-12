90858c810d3e1e5769def4591e46c804fb6c72ac
Yuta Endo <y.endo@noraneko.works>
2017-11-12 19:52:08 +0900

    Revert "s3誤対応の修正"

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    region: 'ap-northeast-1'
+    region: 'ap-northeast-1',
+    use_iam_profile: true
   }
 
   config.fog_directory  = 'gather-photo'
