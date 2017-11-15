a2fd19e83c5063e6d90f63abb8e4cf987dd2fe2f
Yuta Endo <Yuta@YutanoMacBook-Pro.local>
2017-11-15 23:58:38 +0900

    ローカルでよからぬ動きをするので一旦コメントアウト

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+  # config.fog_credentials = {
+  #   provider: 'AWS',
+  #   aws_access_key_id: 'AKIAJEHUIMM5Z7ZRGK2Q',
+  #   aws_secret_access_key: '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',
+  #   region: 'ap-northeast-1'
+  # }
+  #
+  # config.fog_directory  = 'gather-photo'
+  # config.cache_storage = :fog
+end
