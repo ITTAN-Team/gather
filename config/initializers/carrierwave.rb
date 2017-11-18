d5d96d07368007d777523791f3186054f51c6ced
Yuta Endo <Yuta@YutanoMacBook-Pro.local>
2017-11-18 14:45:09 +0900

    S3対応

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,13 @@
 CarrierWave.configure do |config|
-  # config.fog_credentials = {
-  #   provider: 'AWS',
-  #   aws_access_key_id: 'AKIAJEHUIMM5Z7ZRGK2Q',
-  #   aws_secret_access_key: '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',
-  #   region: 'ap-northeast-1'
-  # }
-  #
-  # config.fog_directory  = 'gather-photo'
-  # config.cache_storage = :fog
+  case Rails.env
+    when 'production'
+      config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: ENV['S3_ACCESS_KEY'],
+        aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+        region: 'ap-northeast-1'
+      }
+      config.fog_directory = 'gather-photo'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/gather'
+  end
 end
