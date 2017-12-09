33d42e775112473e41fc096fa47d6e85be832ed2
Yuta Endo <Yuta@YutanoMacBook-Pro.local>
2017-12-09 19:31:11 +0900

    人数のバグを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,6 +14,17 @@ module ApplicationHelper
   def is_leave? status
     return status == 0
   end
+
+  def getParticipant event_users
+    participant = []
+    event_users.each do |user|
+      if user.status == 1 || user.admin
+        participant.push(user)
+      end
+    end
+    return participant.size
+  end
+
   def current_env
     raw ENV["RAILS_ENV"].to_json;
   end
